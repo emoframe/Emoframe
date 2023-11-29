aad44fb5ff315f6c9a42fa3f3fcf503cf5fe4ec3
matheus-sousa007 <mvsousa5@gmail.com>
2023-11-28 21:54:06 -0300

    Adding Sam and Panas Forms backend

---
+++ src/types/forms.ts
@@ -0,0 +1,27 @@
+export interface Sam {
+    satisfaction: string;
+    motivation: string;
+    willpower: string;
+}
+
+export interface Panas {
+    repulsion: string;
+    tormented: string;
+    scared: string;
+    hearty: string;
+    horny: string;
+    guilty: string;
+    enthusiastic: string;
+    pleasantly_surprised: string;
+    disturbed: string;
+    trembling: string;
+    active: string;
+    proud: string;
+    inspired: string;
+    nervous: string;
+    angry: string;
+    determined: string;
+    charmed: string;
+    remorse: string;
+    frightened: string;
+}
