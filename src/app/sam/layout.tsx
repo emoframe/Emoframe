b581e0bbfaa29716bfdff37a2e4e95373c56e554
matheus-sousa007 <mvsousa5@gmail.com>
2023-11-28 21:56:19 -0300

    Adding SAM Form endpoint

---
+++ src/app/sam/layout.tsx
@@ -0,0 +1,17 @@
+import { FC, ReactNode } from 'react';
+import type { Metadata } from 'next'
+
+interface SamLayoutProps {
+  children: ReactNode;
+}
+
+export const metadata: Metadata = {
+  title: "SAM",
+  description: "Formulário Self-Assessment Manikin (SAM)"
+}
+
+const SamLayout: FC<SamLayoutProps> = ({ children }) => {
+    return <div className='flex bg-slate-200 p-10 m-y-10 rounded-md'>{children}</div>;
+}
+
+export default SamLayout;
