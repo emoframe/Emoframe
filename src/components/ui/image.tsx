2b702c14e7e4b659d32d46025eebf7b2479191cb
matheus-sousa007 <mvsousa5@gmail.com>
2023-11-28 21:57:06 -0300

    Adding Image component

---
+++ src/components/ui/image.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import Image, { StaticImageData } from 'next/image'
+
+export interface ImageCardProps {
+    src: StaticImageData | void,
+    alt: string, 
+    width: number,
+    height: number,
+}
+
+const ImageCard = ({src, alt, width, height}: ImageCardProps) => {
+    return (
+        <div className="rounded-sm">
+            <Image src={src}
+            width={width}
+            height={height}
+            alt={alt} />
+        </div>
+    )
+};
+
+export { ImageCard }
