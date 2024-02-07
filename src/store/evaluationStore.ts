f3030672b3b594f0bb95853856b78f0af1cc2548
marcus-v-rodrigues <marcus.v.rodrigues@outlook.com>
2024-02-07 17:43:10 -0300

    Update add function on useEvaluationStore

--- src/store/evaluationStore.ts
+++ src/store/evaluationStore.ts
@@ -3,12 +3,18 @@ import { create } from "zustand";
 
 type EvaluationStore = {
     itens: Evaluation[];
-    add: (item: Evaluation) => void;
+    add: (item: Evaluation | Evaluation[]) => void;
 };
 
 export const useEvaluationStore = create<EvaluationStore>((set) => {
     return {
         itens: [],
-        add: (item) => set((state) => ({ itens: [...state.itens, item] })),
+        add: (item) => {
+            set((state) => ({
+                itens: Array.isArray(item)
+                    ? [...state.itens, ...item]
+                    : [...state.itens, item],
+            }));
+        },
     };
 });
