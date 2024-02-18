2cca9e9ab8d06d8f361d7af8c19e363e033642c2
marcus-v-rodrigues <marcus.v.rodrigues@outlook.com>
2024-02-18 15:49:10 -0300

    WIP: Improve stepper application in PanasForm

--- src/components/form/instrument/PanasForm.tsx
+++ src/components/form/instrument/PanasForm.tsx
@@ -149,27 +149,29 @@ const PanasForm = ({userId, evaluationId}: FillEvaluationForm) => {
     })
 
     if(!isReady) return null;
-    switch (activeStep) {
-        case 0:
-            return (
-                <React.Suspense fallback={<Progress />}>
+    return (
+        <div>
+            <Steps activeStep={activeStep}>
+                {steps.map((step, index) => ( <Step index={index} key={index} additionalClassName={{label: "text-md"}} {...step} /> ))}
+            </Steps>
+           
+            <div className="flex flex-col flex-wrap justify-center gap-8">
+                
+                <h1 className="font-bold text-4xl self-center"> PANAS </h1>
+                <h2 className="text-md self-center"> Esta escala consiste num conjunto de palavras que descrevem diferentes sentimentos e emoções. Leia cada palavra e marque a resposta adequada a palavra. Veja a escala e exemplos de preenchimento abaixo: </h2>
+                <div className="flex flex-row justify-around">
+                    <Button className="text-lg basis-1/3" type="button" size="lg">Exemplos</Button>
+                </div>
+                <div>
+                    <hr className="text-black bg-slate-300 h-0.5"/>
+                </div>
+                <h2 className="text-md self-center"> Indique em que medida está sentindo cada uma das emoções AGORA: </h2>
+
+                <React.Suspense key={activeStep} fallback={<Progress />}>
                     <Form key={activeStep} {...form}>
-                        <Steps activeStep={activeStep}>
-                            {steps.map((step, index) => ( <Step index={index} key={index} additionalClassName={{label: "text-md"}} {...step} /> ))}
-                        </Steps>
-                        <form key={activeStep}>
-                            <div className="flex flex-col flex-wrap justify-center gap-8">
-                                <h1 className="font-bold text-4xl self-center"> PANAS </h1>
-                                <h2 className="text-md self-center"> Esta escala consiste num conjunto de palavras que descrevem diferentes sentimentos e emoções. Leia cada palavra e marque a resposta adequada a palavra. Veja a escala e exemplos de preenchimento abaixo: </h2>
-                                <div className="flex flex-row justify-around">
-                                    <Button className="text-lg basis-1/3" type="button" size="lg">Exemplos</Button>
-                                </div>
-                                <div>
-                                    <hr className="text-black bg-slate-300 h-0.5"/>
-                                </div>
-                                <h2 className="text-md self-center"> Indique em que medida está sentindo cada uma das emoções AGORA: </h2>
-                                {PanasQuestions[activeStep].map((question, index) => (
-                                    <>
+                        {
+                            PanasQuestions[activeStep].map((question, index) => (
+                                <>
                                     <FormField key={"formField" + index}
                                     control={form.control}
                                     name={question.field}
@@ -200,98 +202,46 @@ const PanasForm = ({userId, evaluationId}: FillEvaluationForm) => {
                                     <div key={"div" + index}>
                                         <hr className="text-black bg-black h-0.5"/>
                                     </div>    
-                                    </>
-                                ))}
-                                <div key="buttons" className="flex flex-row justify-around mt-8">
-                                    <Button className="basis-1/8 text-lg" type='button' size="lg" onClick={() => {
-                                        PanasQuestions[activeStep].map((question, index) => (form.setValue(question.field, '')));
-                                        window.scrollTo({top: 0, left: 0, behavior: "smooth"});
-                                    }}>Limpar</Button>
-                                    <Button className="basis-1/8 text-lg" type="button" size="lg" onClick={() => {
-                                        const values = form.getValues(PanasQuestions[activeStep].map((question, index) => (question.field)));
-                                        const hasNull = Object.values(values).some((value) => value === "");
-                                        
-                                        if (hasNull) {
-                                            alert("Preencha todos os campos!");
-                                        }
-                                        else{
-                                        nextStep();
-                                    }}}>Próximo</Button>
-                                </div>
-                            </div>
-                        </form>
-                    </Form>
-                    </React.Suspense>
-                )
-        case 1: 
-            return (
-                <React.Suspense fallback={<Progress />}>
-                <Form key={activeStep} {...form}>
-                    <Steps activeStep={activeStep}>
-                        {steps.map((step, index) => ( <Step index={index} key={index} {...step} /> ))}
-                    </Steps>
-                    <form key={activeStep} onSubmit={form.handleSubmit(onSubmit)}>
-                        <div className="flex flex-col flex-wrap justify-center gap-8">
-                            <h1 className="font-bold text-4xl self-center"> PANAS </h1>
-                            <h2 className="text-md self-center"> Esta escala consiste num conjunto de palavras que descrevem diferentes sentimentos e emoções. Leia cada palavra e marque a resposta adequada a palavra. Veja a escala e exemplos de preenchimento abaixo: </h2>
-                            <div className="flex flex-row justify-around">
-                                <Button className="text-lg basis-1/3" type="button" size="lg">Exemplos</Button>
-                            </div>
-                            <div>
-                                <hr className="text-black bg-slate-300 h-0.5"/>
-                            </div>
-                            <h2 className="text-md self-center"> Indique em que medida está sentindo cada uma das emoções AGORA: </h2>
-                            {PanasQuestions[activeStep].map((question, index) => (
-                                <>
-                                <FormField
-                                control={form.control}
-                                name={question.field}
-                                render={({field}) => (
-                                    <FormItem className="space-x-5 space-y-5 content-center">
-                                    <p className="text-2xl"><b>{question.question}</b></p>
-                                    <FormControl>
-                                        <RadioGroup
-                                        onValueChange={field.onChange}
-                                        defaultValue={field.value}
-                                        value={field.value}
-                                        className="flex flex-row space-x-5 justify-between">
-                                            {DefaultProps.map((defaultProp, index) => (
-                                                <FormItem className="flex flex-col items-center space-y-2" key={index}>
-                                                    <FormControl>
-                                                        <RadioGroupItem value={defaultProp.value}/> 
-                                                    </FormControl>
-                                                    <FormLabel className="font-normal">
-                                                        {defaultProp.label}
-                                                    </FormLabel>
-                                                </FormItem>
-                                            ))}
-                                        </RadioGroup>
-                                        </FormControl>
-                                        <FormMessage />
-                                </FormItem>
-                                )} />
-                                <div key={index}>
-                                    <hr className="text-black bg-black h-0.5"/>
-                                </div>    
                                 </>
-                            ))}
-                            <div className="flex flex-row justify-around mt-8">
-                                <Button className="basis-1/8 text-lg" type="button" size="lg" onClick={() => {    
-                                    prevStep();
-                                    window.scrollTo({top: 0, left: 0, behavior: "smooth"});
-                                }}>Anterior</Button>
-                                <Button className="basis-1/8 text-lg" type='button' onClick={() => {
-                                    PanasQuestions[activeStep].map((question, index) => (form.setValue(question.field, '')));
-                                    window.scrollTo({top: 0, left: 0, behavior: "smooth"});
-                                }}>Limpar</Button>
-                                <Button className="basis-1/8 text-lg" type="submit">Enviar</Button>
-                            </div>
-                        </div>
-                    </form>
-                </Form>
+                            ))
+                        }
+                    </Form>
                 </React.Suspense>
-            )
-    }
+
+                <div key="buttons" className="flex flex-row justify-around mt-8">
+                    { 
+                        (activeStep != 0) && 
+                        <Button className="basis-1/8 text-lg" type="button" size="lg" onClick={() => {    
+                            prevStep();
+                            window.scrollTo({top: 0, left: 0, behavior: "smooth"});
+                        }}>Anterior</Button>
+                    }
+                    
+                    <Button className="basis-1/8 text-lg" type='button' size="lg" onClick={() => {
+                        PanasQuestions[activeStep].map((question, index) => (form.setValue(question.field, '')));
+                        window.scrollTo({top: 0, left: 0, behavior: "smooth"});
+                    }}>Limpar</Button>
+
+
+                    { 
+                        (activeStep != 1) ?
+                        <Button className="basis-1/8 text-lg" type="button" size="lg" onClick={() => {
+                            const values = form.getValues(PanasQuestions[activeStep].map((question, index) => (question.field)));
+                            const hasNull = Object.values(values).some((value) => value === "");
+                            
+                            if (hasNull) {
+                                alert("Preencha todos os campos!");
+                            }
+                            else{
+                            nextStep();
+                        }}}>Próximo</Button>
+                        : <Button className="basis-1/8 text-lg" type="submit" size="lg">Enviar</Button>
+                    }
+
+                </div>
+            </div>
+       </div>
+    );
 }
 
 export default PanasForm
