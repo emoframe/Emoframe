5f39d5afa19fe4b5b1eab997d99ea53051d86e66
marcus-v-rodrigues <marcus.v.rodrigues@outlook.com>
2024-03-22 19:22:38 -0300

    Update user main page cards

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -14,12 +14,13 @@ import { authOptions } from "../../../pages/api/auth/[...nextauth]";
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 import { instruments } from "@/types/forms";
+import { OptionCard, Content } from "@/components/OptionCard";
 
 //Resolve o problema de cache após atualização
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-const User = async () => {
+const EvaluationsCards = async () => {
   const session: any = await getServerSession(authOptions);
 
   const parameters: Search = {
@@ -41,38 +42,30 @@ const User = async () => {
         .localeCompare(b.identification.toLowerCase())
     );
 
+  const transformedContent: Content[] = evaluations.map(evaluation => ({
+    title: evaluation.identification,
+    description: `${instruments.find((instrument) => instrument.value === evaluation.instrument)?.label} - ${evaluation.method}`,
+    href: `/user/evaluations/fill?evaluation=${evaluation.uid}`
+  }));
+
   return (
     <>
       {evaluations.length ? (
-        <div className="grid grid-flow-row gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {evaluations.map((evaluation) => {
-            return (
-              <Card
-                key={evaluation.uid}
-                className="rounded shadow-2xl shadow-shadow_color bg-primary-background border-none duration-300 hover:-translate-y-3"
-              >
-                <CardHeader>
-                  <CardTitle>{evaluation.identification}</CardTitle>
-                  <CardDescription>{`${instruments.find((instrument) => instrument.value === evaluation.instrument)?.label} - ${evaluation.method}`}</CardDescription>
-                </CardHeader>
-                <CardContent className="flex justify-center">
-                  <Link
-                    className={buttonVariants({ variant: "default" })}
-                    href={`/user/evaluations/fill?evaluation=${evaluation.uid}`}
-                    replace
-                  >
-                    Acessar
-                  </Link>
-                </CardContent>
-              </Card>
-            );
-          })}
-        </div>
-      ) : (
-        <p>Não há avaliações disponíveis</p>
-      )}
+        transformedContent.map((content, index) => (
+          <OptionCard key={index} content={content} />
+        ))
+      ) : <p>Não há avaliações disponíveis</p>}
     </>
   );
 };
+
+
+const User = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <EvaluationsCards/>
+    </div>
+  )
+}
 
 export default User;
