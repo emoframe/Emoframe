65f94bb1d3cc7f85262bac1ee2a074e99a867694
matheus-sousa007 <mvsousa5@gmail.com>
2024-03-22 20:53:24 -0300

    Adding new page for PDFView visualization

--- src/app/specialist/results/pdf-view/page.tsx
+++ src/app/specialist/results/pdf-view/page.tsx
@@ -50,11 +50,7 @@ const PDFViewPage = () => {
 
   if(!user || !formData || !specialistName || !age) return null
   return (
-<<<<<<< HEAD
-      <PDFView user={user} specialist={specialistName} age={age} fid={eid} data={formData} type={type} />
-=======
       <PDFView user={user} specialist={specialistName} age={age} fid={eid} data={formData} type={type} />  
->>>>>>> feature/acessibility-menu
   )
 }
 
