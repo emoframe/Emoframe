519a889140691a58e03564ab43af9ffaeefb75fc
marcus-v-rodrigues <marcus.v.rodrigues@outlook.com>
2024-03-22 10:37:33 -0300

    Create OptionCard

--- src/app/specialist/services/page.tsx
+++ src/app/specialist/services/page.tsx
@@ -1,47 +1,16 @@
-import { Button } from '@/components/ui/button';
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { ArrowRight } from 'lucide-react';
-import Link from 'next/link';
+import { OptionCard, Content } from '@/components/OptionCard';
 import React from 'react'
 
-type Content = {
-  title: string,
-  description: string,
-  href: string,
-}
-
 const contents: Content[] = [
-  {title: "Instrumentos", description: "Lorem Ipsum", href: "instruments"},
-  {title: "Modelos", description: "Lorem Ipsum", href: "templates"},
+  {title: "Instrumentos", description: "Lorem Ipsum", href: "/specialist/services/instruments"},
+  {title: "Modelos", description: "Lorem Ipsum", href: "/specialist/services/templates"},
 ]
-
-const ServiceCard = ({ content }: { content: Content }) => {
-  return (
-    <Card className="shadow-2xl shadow-shadow_color bg-primary-background border-background border-2 duration-300 hover:-translate-y-3">
-      <CardHeader>
-        <CardTitle className="flex items-center gap-2 justify-between">
-          <span className="truncate font-bold">{content.title}</span>
-        </CardTitle>
-        <CardDescription className="flex items-center justify-between text-muted-foreground text-sm">
-          {content.description}
-        </CardDescription>
-      </CardHeader>
-      <CardFooter>
-        <Button asChild className="w-full mt-2 text-md gap-4">
-          <Link href={`/specialist/services/${content.href}`}>
-            Acessar <ArrowRight />
-          </Link>
-        </Button>
-      </CardFooter>
-    </Card>
-  );
-}
 
 const ServiceCards = () => {
   return (
     <>
       {contents.map((content, index) => (
-        <ServiceCard key={index} content={content} />
+        <OptionCard key={index} content={content} />
       ))}
     </>
   );
---
+++ src/components/OptionCard.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { ArrowRight } from "lucide-react";
+import { Button } from "./ui/button";
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import Link from "next/link";
+
+export type Content = {
+  title: string,
+  description: string,
+  href: string,
+}
+
+export const OptionCard = ({ content }: { content: Content }) => {
+  return (
+    <Card className="shadow-2xl shadow-shadow_color bg-primary-background border-background border-2 duration-300 hover:-translate-y-3">
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2 justify-between">
+          <span className="truncate font-bold">{content.title}</span>
+        </CardTitle>
+        <CardDescription className="flex items-center justify-between text-muted-foreground text-sm">
+          {content.description}
+        </CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <Button asChild className="w-full mt-2 text-md gap-4">
+          <Link href={content.href}>
+            Acessar <ArrowRight />
+          </Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
