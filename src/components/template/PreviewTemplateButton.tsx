89a765cfdf0ef3d01f7b294c81588251b78b1291
marcus-v-rodrigues <marcus.v.rodrigues@outlook.com>
2024-03-28 11:27:54 -0300

    WIP: Create PreviewDialogButton

---
+++ src/components/template/PreviewTemplateButton.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import useDesigner from "@/hooks/useDesigner";
+import { ScanEye } from 'lucide-react';
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+
+const PreviewDialogButton = () => {
+  const { elements } = useDesigner();
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={"outline"} className="gap-2">
+          <ScanEye className="h-6 w-6" />
+          Preview
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="w-screen h-screen max-h-screen max-w-full flex flex-col flex-grow p-0 gap-0">
+        <div className="px-4 py-2 border-b">
+          <p className="text-lg font-bold text-muted-foreground">Visualização de Template</p>
+          <p className="text-sm text-muted-foreground">É assim que o este template aparecerá para os usuários</p>
+        </div>
+        <div className="bg-accent flex flex-col flex-grow items-center justify-center p-4 bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)] overflow-y-auto">
+          <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background h-full w-full rounded-2xl p-8 overflow-y-auto">
+            {elements.map((element) => {
+              const FormComponent = FormElements[element.type].formComponent;
+              return <FormComponent key={element.id} elementInstance={element} />;
+            })}
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default PreviewDialogButton;
