33e52410df29c276e80c767db07427b14d2958f8
matheus-sousa007 <mvsousa5@gmail.com>
2024-04-02 17:35:14 -0300

    WIP: Adding loading component

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -5,6 +5,14 @@ import * as ProgressPrimitive from "@radix-ui/react-progress"
 
 import { cn } from "@/lib/utils"
 
+import { Label } from '@/components/ui/label';
+
+export interface LoadingProps extends React.HTMLAttributes<HTMLDivElement> {
+  label?: string,
+  value?: number
+}
+
+
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
@@ -18,11 +26,27 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className="h-full w-full flex-1 bg-primary-foreground transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
 ))
 Progress.displayName = ProgressPrimitive.Root.displayName
 
-export { Progress }
+const Loading = React.forwardRef<
+    HTMLDivElement,
+    LoadingProps
+>(({ className, value, label, ...props }, ref) => (
+      <div ref={ref} className={cn('flex flex-col items-center justify-center w-full h-full py-5', className)} {...props}>
+        <Progress value={value} className='w-[150%] h-5' />
+        <Label className='text-[20px]'> {label} </Label>
+      </div>
+))
+
+Loading.displayName = "Loading"
+
+    
+export { 
+  Progress,
+  Loading
+}
