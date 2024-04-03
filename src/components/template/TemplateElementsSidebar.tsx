d6e762026d2edf8405ec05bc7690077b4d455ed3
marcus-v-rodrigues <marcus.v.rodrigues@outlook.com>
2024-04-03 10:19:39 -0300

    WIP: Create TemplateElementsSidebar

---
+++ src/components/template/TemplateElementsSidebar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import SidebarButtonElement from "@/components/template/SidebarButtonElement";
+import { TemplateElements } from "@/components/template/TemplateElements";
+import { Separator } from "@/components/ui/separator";
+
+const TemplateElementsSidebar = () => {
+  return (
+    <div>
+      <p className="text-sm text-foreground/70">Elementos Arrasta e Solta</p>
+      <Separator className="my-2" />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 place-items-center">
+        <p className="text-sm text-muted-foreground col-span-1 md:col-span-2 my-2 place-self-start">Elementos de Layout</p>
+        <SidebarButtonElement formElement={TemplateElements.TitleField} />
+
+        <p className="text-sm text-muted-foreground col-span-1 md:col-span-2 my-2 place-self-start">Elementos de Formulário</p>
+        {/*TODO */}
+      </div>
+    </div>
+  );
+}
+
+export default TemplateElementsSidebar
