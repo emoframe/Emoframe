17793bc75c4f9c81ef64e2e5a0af623433c43062
marcus-v-rodrigues <marcus.v.rodrigues@outlook.com>
2024-04-09 17:54:09 -0300

    Fix: Global layout vertical padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
                     <body className={cn("flex flex-1 w-full min-h-full h-fit bg-background", inter.className)}>
                         <ThemeProvider attribute="class" defaultTheme="light" disableTransitionOnChange>
                             <Sidebar/>
-                            <main className="flex flex-1 flex-col justify-center items-center pl-sidebar py-16">
+                            <main className="flex flex-1 flex-col justify-center items-center pl-sidebar">
                                 {children}
                             </main>
                             <Toaster/>
