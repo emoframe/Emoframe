b2715338f69404bc77d48bfd688b59a23d5a296f
marcus-v-rodrigues <marcus.v.rodrigues@outlook.com>
2024-04-09 19:38:32 -0300

    Update text color in PreviewTemplate

--- src/components/template/PreviewTemplateButton.tsx
+++ src/components/template/PreviewTemplateButton.tsx
@@ -18,12 +18,12 @@ const PreviewTemplateButton = () => {
         </Button>
       </DialogTrigger>
       <DialogContent className="w-screen h-screen max-h-screen max-w-full flex flex-col flex-grow p-0 gap-0">
-        <div className="px-4 py-2 border-b">
-          <p className="text-lg font-bold text-muted-foreground">Visualização de Template</p>
-          <p className="text-sm text-muted-foreground">É assim que o este template aparecerá para os usuários</p>
+        <div className="px-4 py-2 border-b bg-primary-background">
+          <p className="text-lg font-bold">Visualização de Template</p>
+          <p className="text-sm">É assim que o este template aparecerá para os usuários</p>
         </div>
-        <div className="bg-accent flex flex-col flex-grow items-center justify-center p-4 bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)] overflow-y-auto">
-          <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background h-full w-full rounded-2xl p-8 overflow-y-auto">
+        <div className="bg-accent flex flex-col flex-grow items-center justify-center p-4 overflow-y-auto">
+          <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-primary-background h-full w-full rounded-2xl p-8 overflow-y-auto">
             {elements.map((element) => {
               const TemplateElement = TemplateElements[element.type].templateComponent;
               return <TemplateElement key={element.id} elementInstance={element} />;
