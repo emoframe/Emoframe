3636489d865f3bcaf52fb540066d21dd92d99eba
marcus-v-rodrigues <marcus.v.rodrigues@outlook.com>
2024-05-12 20:12:48 -0300

    WIP: Implement TemplateForm

---
+++ src/components/form/instrument/TemplateForm.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import React, { useCallback, useRef, useState, useTransition } from "react";
+import { TemplateElementInstance, TemplateElements } from "@/components/template/TemplateElements";
+import { Button } from "@/components/ui/button";
+import { HiCursorClick } from "react-icons/hi";
+import { toast } from "@/components/ui/use-toast";
+import { Loader2 } from "lucide-react";
+
+function TemplateForm({ formUrl, content }: { content: TemplateElementInstance[]; formUrl: string }) {
+  const formValues = useRef<{ [key: string]: string }>({});
+  const formErrors = useRef<{ [key: string]: boolean }>({});
+  const [renderKey, setRenderKey] = useState(new Date().getTime());
+
+  const [submitted, setSubmitted] = useState(false);
+  const [pending, startTransition] = useTransition();
+
+  const validateForm: () => boolean = useCallback(() => {
+    for (const field of content) {
+      const actualValue = formValues.current[field.id] || "";
+      const valid = TemplateElements[field.type].validate(field, actualValue);
+
+      if (!valid) {
+        formErrors.current[field.id] = true;
+      }
+    }
+
+    if (Object.keys(formErrors.current).length > 0) {
+      return false;
+    }
+
+    return true;
+  }, [content]);
+
+  const submitValue = useCallback((key: string, value: string) => {
+    formValues.current[key] = value;
+  }, []);
+
+  const submitForm = async () => {
+    formErrors.current = {};
+    const validForm = validateForm();
+    if (!validForm) {
+      setRenderKey(new Date().getTime());
+      toast({
+        title: "Error",
+        description: "please check the form for errors",
+        variant: "destructive",
+      });
+      return;
+    }
+
+    try {
+      // TODO: submit form
+      setSubmitted(true);
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Something went wrong",
+        variant: "destructive",
+      });
+    }
+  };
+
+  if (submitted) {
+    return (
+      <div className="flex justify-center w-full h-full items-center p-8">
+        <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-blue-700 rounded">
+          <h1 className="text-2xl font-bold">Avaliação enviada</h1>
+          <p className="text-muted-foreground">Obrigado por enviar o formulário, você pode fechar esta página agora.</p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex justify-center w-full h-full items-center p-8">
+      <div
+        key={renderKey}
+        className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-blue-700 rounded"
+      >
+        {content.map((element) => {
+          const FormElement = TemplateElements[element.type].templateComponent;
+          return (
+            <FormElement
+              key={element.id}
+              elementInstance={element}
+              submitValue={submitValue}
+              isInvalid={formErrors.current[element.id]}
+              defaultValue={formValues.current[element.id]}
+            />
+          );
+        })}
+        <Button
+          className="mt-8"
+          onClick={() => {
+            startTransition(submitForm);
+          }}
+          disabled={pending}
+        >
+          {!pending && (
+            <>
+              <HiCursorClick className="mr-2" />
+              Enviar
+            </>
+          )}
+          {pending && <Loader2 className="animate-spin" />}
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default TemplateForm;
