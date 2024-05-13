bc07c62b73572a948609821ad1847f177bd8d37a
marcus-v-rodrigues <marcus.v.rodrigues@outlook.com>
2024-05-13 02:01:34 -0300

    Implement isSameDay function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -123,6 +123,12 @@ export function convertTemplatesToOptions(templates: Template[]): Option[] {
   }));
 };
 
+export function isSameDay(date1: Date, date2: Date): boolean {
+  return date1.getDate() === date2.getDate() &&
+         date1.getMonth() === date2.getMonth() &&
+         date1.getFullYear() === date2.getFullYear();
+}
+
 export function differentiateKeysFromObjects(keepObject: Object, changeObject: Object, suffixKey: string): void {
   Object.keys(changeObject).forEach((changeObjectKey) => {
     Object.keys(keepObject).forEach((keepObjectKey) => {
